5a2a9b7a6a50fc959f970e2c58cd130e4bcd9af0
Bruno Cabral <cabral.brc@gmail.com>
2022-04-07 12:57:15 -0700

    detect faces and show

--- main.py
+++ main.py
@@ -23,6 +23,19 @@ while True:
 
     _, frame = video.read()
 
+    # detect faces
+    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(
+        img_gray, scaleFactor=1.3, minNeighbors=7, minSize=(50, 50)
+    )
+
+    # update text
+    window["-TEXT-"].update(f"People in picture: {len(faces)}")
+
+    # draw rectangles
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
     # update the image
     img_bytes = cv2.imencode(".png", frame)[1].tobytes()
     window["-IMAGE-"].update(img_bytes)
