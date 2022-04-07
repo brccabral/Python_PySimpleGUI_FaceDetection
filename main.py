6434a427370a226f05436fec2f26d581ebe303dc
Bruno Cabral <cabral.brc@gmail.com>
2022-04-07 12:48:58 -0700

    open camera

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import PySimpleGUI as sg
+import cv2
 
-
-layout = [[]]
+layout = [
+    [sg.Image("", key="-IMAGE-", background_color="white")],
+    [
+        sg.Text(
+            "People in picture: 0", key="-TEXT-", justification="center", expand_x=True
+        )
+    ],
+]
 
 window = sg.Window("Face Detection", layout)
 
+# get video
+video = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=0)
     if event == sg.WIN_CLOSED:
         break
+
+    _, frame = video.read()
+
+    # update the image
+    img_bytes = cv2.imencode(".png", frame)[1].tobytes()
+    window["-IMAGE-"].update(img_bytes)
 
 window.close()
